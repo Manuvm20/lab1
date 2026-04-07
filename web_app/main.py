6855b338c3db815afc1ecbc764ca9f34fda62e7e
Manuvm20 <24aiml006@bnmit.in>
2026-04-07 09:25:06 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -1,1 +1,9 @@
+main.py
+form flask import flask,render_template
+app=flask(__name__)
+@app.route('/')
+def home();
+return render_template('index.html',file="home page")
+if__name__=='__main__':
+app.run(host='127.0.0.1',port=8080,debug=true)
 
