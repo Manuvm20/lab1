8616bd378745786f4fb6f527611b417913d07760
Manuvm20 <24aiml006@bnmit.in>
2026-04-07 09:40:31 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -4,6 +4,15 @@ app=flask(__name__)
 @app.route('/')
 def home();
 return render_template('index.html',file="home page")
-if__name__=='__main__':
-app.run(host='127.0.0.1',port=8080,debug=true)
+if__name__=='__main__':from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Note: Use a colon (:) after the function name, not a semicolon (;)
+    return render_template('index.html', file="home page")
+
+if __name__ == '__main__':
+    # 'True' must be capitalized in Python
+    app.run(host='127.0.0.1', port=8080, debug=True)
