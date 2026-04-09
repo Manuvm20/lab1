cd28260a66ad7a4a53e8b67b15fc5843bf73558b
Manuvm20 <24aiml006@bnmit.in>
2026-04-09 18:44:56 +0530

    Update main.py

--- web_app/main.py
+++ web_app/main.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Note: Use a colon (:) after the function name, not a semicolon (;)
     return render_template('index.html', file="home page")
 
 if __name__ == '__main__':
-    # 'True' must be capitalized in Python
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(debug=True)
